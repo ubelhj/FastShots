fef83f6a143eb7e874ad6a5af9801f4957e7e6f6
ubelhj <ubelhj@uw.edu>
2021-07-11 11:58:46 -0700

    renames to Fast or Slow Goal Filter

--- FastShots/FastShots.cpp
+++ FastShots/FastShots.cpp
@@ -2,7 +2,7 @@
 #include "FastShots.h"
 
 
-BAKKESMOD_PLUGIN(FastShots, "Fast Shots Plugin", plugin_version, PLUGINTYPE_FREEPLAY)
+BAKKESMOD_PLUGIN(FastShots, "Fast or Slow Goal Filter", plugin_version, PLUGINTYPE_FREEPLAY)
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 float blueMinSpeed = 100.f;
@@ -11,6 +11,8 @@ float blueMaxSpeed = 220.f;
 float orangeMaxSpeed = 220.f;
 bool blueEnabled = false;
 bool orangeEnabled = false;
+
+int backWall = 5040;
 
 bool setBlueMin = false;
 bool setOrangeMin = false;
@@ -23,7 +25,7 @@ void FastShots::onLoad()
 {
 	_globalCvarManager = cvarManager;
 
-	cvarManager->registerCvar("fast_shots_enabled", "0", "Enable the fast shots plugin", true, true, 0, true, 1)
+	cvarManager->registerCvar("fsgf_enabled", "0", "Enable the Fast or Slow Goal Filter plugin", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 		if (cvar.getBoolValue()) {
 			hookEvents();
@@ -33,107 +35,134 @@ void FastShots::onLoad()
 		}
 			});
 
-	cvarManager->registerCvar("fast_shots_blue", "0", "makes the blue team take fast shots", true, true, 0, true, 1)
+	cvarManager->registerCvar("fsgf_blue", "0", "makes the blue team take fast shots", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) { blueEnabled = cvar.getBoolValue(); });
 
-	cvarManager->registerCvar("fast_shots_min_blue", std::to_string(blueMaxSpeed), "minimum shot speed allowed for blue")
+	cvarManager->registerCvar("fsgf_min_blue", std::to_string(blueMaxSpeed), "minimum shot speed allowed for blue")
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 		blueMinSpeed = cvar.getFloatValue();
 			});
 
-	cvarManager->registerCvar("fast_shots_orange", "0", "makes the orange team take fast shots", true, true, 0, true, 1)
+	cvarManager->registerCvar("fsgf_orange", "0", "makes the orange team take fast shots", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) { orangeEnabled = cvar.getBoolValue(); });
 
-	cvarManager->registerCvar("fast_shots_min_orange", std::to_string(orangeMinSpeed), "minimum shot speed allowed for orange")
+	cvarManager->registerCvar("fsgf_min_orange", std::to_string(orangeMinSpeed), "minimum shot speed allowed for orange")
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 		orangeMinSpeed = cvar.getFloatValue();
 			});
 
-	cvarManager->registerCvar("fast_shots_max_orange", std::to_string(orangeMaxSpeed), "maximum shot speed allowed for orange")
+	cvarManager->registerCvar("fsgf_max_orange", std::to_string(orangeMaxSpeed), "maximum shot speed allowed for orange")
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 		orangeMaxSpeed = cvar.getFloatValue();
 			});
 
-	cvarManager->registerCvar("fast_shots_max_blue", std::to_string(blueMaxSpeed), "maximum shot speed allowed for blue")
+	cvarManager->registerCvar("fsgf_max_blue", std::to_string(blueMaxSpeed), "maximum shot speed allowed for blue")
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 		blueMaxSpeed = cvar.getFloatValue();
 			});
 
-	cvarManager->registerCvar("fast_shots_update_min_blue", std::to_string(setBlueMin), 
+	cvarManager->registerCvar("fsgf_update_min_blue", std::to_string(setBlueMin), 
 		"updates the blue team's min shot speed", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) { 
 			setBlueMin = cvar.getBoolValue(); 
 
 			if (setBlueMin) {
-				cvarManager->executeCommand("fast_shots_update_min_all 0");
+				cvarManager->executeCommand("fsgf_update_min_all 0");
 
-				cvarManager->executeCommand("fast_shots_update_max_blue 0");
-				cvarManager->executeCommand("fast_shots_update_max_all 0");
+				cvarManager->executeCommand("fsgf_min_blue 0");
+				cvarManager->executeCommand("fsgf_max_blue 220");
+
+				cvarManager->executeCommand("fsgf_update_max_blue 0");
+				cvarManager->executeCommand("fsgf_update_max_all 0");
 			}});
 
-	cvarManager->registerCvar("fast_shots_update_min_orange", std::to_string(setOrangeMin),
+	cvarManager->registerCvar("fsgf_update_min_orange", std::to_string(setOrangeMin),
 		"updates the orange team's min shot speed", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 			setOrangeMin = cvar.getBoolValue();
 
 			if (setOrangeMin) {
-				cvarManager->executeCommand("fast_shots_update_min_all 0");
+				cvarManager->executeCommand("fsgf_update_min_all 0");
 
-				cvarManager->executeCommand("fast_shots_update_max_orange 0");
-				cvarManager->executeCommand("fast_shots_update_max_all 0");
+				cvarManager->executeCommand("fsgf_min_orange 0");
+				cvarManager->executeCommand("fsgf_max_orange 220");
+
+				cvarManager->executeCommand("fsgf_update_max_orange 0");
+				cvarManager->executeCommand("fsgf_update_max_all 0");
 			}});
 
-	cvarManager->registerCvar("fast_shots_update_min_all", std::to_string(setBothMin),
+	cvarManager->registerCvar("fsgf_update_min_all", std::to_string(setBothMin),
 		"updates the whole lobby's min shot speed", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 			setBothMin = cvar.getBoolValue();
 
 			if (setBothMin) {
-				cvarManager->executeCommand("fast_shots_update_min_blue 0");
-				cvarManager->executeCommand("fast_shots_update_min_orange 0");
+				cvarManager->executeCommand("fsgf_update_min_blue 0");
+				cvarManager->executeCommand("fsgf_update_min_orange 0");
 
-				cvarManager->executeCommand("fast_shots_update_max_blue 0");
-				cvarManager->executeCommand("fast_shots_update_max_orange 0");
-				cvarManager->executeCommand("fast_shots_update_max_all 0");
+				cvarManager->executeCommand("fsgf_min_blue 0");
+				cvarManager->executeCommand("fsgf_max_blue 220");
+				cvarManager->executeCommand("fsgf_min_orange 0");
+				cvarManager->executeCommand("fsgf_max_orange 220");
+
+				cvarManager->executeCommand("fsgf_update_max_blue 0");
+				cvarManager->executeCommand("fsgf_update_max_orange 0");
+				cvarManager->executeCommand("fsgf_update_max_all 0");
 		}});
 
-	cvarManager->registerCvar("fast_shots_update_max_blue", std::to_string(setBlueMin),
+	cvarManager->registerCvar("fsgf_update_max_blue", std::to_string(setBlueMin),
 		"updates the blue team's max shot speed", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 			setBlueMax = cvar.getBoolValue();
 
 			if (setBlueMax) {
-				cvarManager->executeCommand("fast_shots_update_min_blue 0");
-				cvarManager->executeCommand("fast_shots_update_min_all 0");
+				cvarManager->executeCommand("fsgf_update_min_blue 0");
+				cvarManager->executeCommand("fsgf_update_min_all 0");
 
-				cvarManager->executeCommand("fast_shots_update_max_all 0");
+				cvarManager->executeCommand("fsgf_min_blue 0");
+				cvarManager->executeCommand("fsgf_max_blue 220");
+
+				cvarManager->executeCommand("fsgf_update_max_all 0");
 			}});
 
-	cvarManager->registerCvar("fast_shots_update_max_orange", std::to_string(setOrangeMin),
+	cvarManager->registerCvar("fsgf_update_max_orange", std::to_string(setOrangeMin),
 		"updates the orange team's max shot speed", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 			setOrangeMax = cvar.getBoolValue();
 
 			if (setOrangeMax) {
-				cvarManager->executeCommand("fast_shots_update_min_orange 0");
-				cvarManager->executeCommand("fast_shots_update_min_all 0");
+				cvarManager->executeCommand("fsgf_update_min_orange 0");
+				cvarManager->executeCommand("fsgf_update_min_all 0");
 
-				cvarManager->executeCommand("fast_shots_update_max_all 0");
+				cvarManager->executeCommand("fsgf_min_orange 0");
+				cvarManager->executeCommand("fsgf_max_orange 220");
+
+				cvarManager->executeCommand("fsgf_update_max_all 0");
 			}});
 
-	cvarManager->registerCvar("fast_shots_update_max_all", std::to_string(setBothMin),
+	cvarManager->registerCvar("fsgf_update_max_all", std::to_string(setBothMin),
 		"updates the whole lobby's max shot speed", true, true, 0, true, 1)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
 			setBothMax = cvar.getBoolValue();
 
 			if (setBothMax) {
-				cvarManager->executeCommand("fast_shots_update_min_blue 0");
-				cvarManager->executeCommand("fast_shots_update_min_orange 0");
-				cvarManager->executeCommand("fast_shots_update_min_all 0");
+				cvarManager->executeCommand("fsgf_update_min_blue 0");
+				cvarManager->executeCommand("fsgf_update_min_orange 0");
+				cvarManager->executeCommand("fsgf_update_min_all 0");
 
-				cvarManager->executeCommand("fast_shots_update_max_blue 0");
-				cvarManager->executeCommand("fast_shots_update_max_orange 0");
+				cvarManager->executeCommand("fsgf_min_blue 0");
+				cvarManager->executeCommand("fsgf_max_blue 220");
+				cvarManager->executeCommand("fsgf_min_orange 0");
+				cvarManager->executeCommand("fsgf_max_orange 220");
+
+				cvarManager->executeCommand("fsgf_update_max_blue 0");
+				cvarManager->executeCommand("fsgf_update_max_orange 0");
 			}});
+
+	cvarManager->registerCvar("fsgf_back_wall", std::to_string(backWall), "sets back wall's location")
+		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
+			backWall = cvar.getIntValue();
+		});
 
 	gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", [this](...) {
 		setNewSpeed();
@@ -193,7 +222,8 @@ void FastShots::onTick(CarWrapper caller) {
 	speed *= 0.036f;
 	speed += 0.5f;
 
-	if (ballLoc.Y > 5028 && velocity.Y > 0) {
+	//  5028
+	if (ballLoc.Y > backWall && velocity.Y > 0) {
 		// ball is going in orange net
 		cvarManager->log("shot taken at orange net " + std::to_string(speed));
 		if (!blueEnabled) {
@@ -203,7 +233,7 @@ void FastShots::onTick(CarWrapper caller) {
 			velocity.Y = -velocity.Y;
 			ball.SetVelocity(velocity);
 		}
-	} else if (ballLoc.Y < -5028 && velocity.Y < 0) {
+	} else if (ballLoc.Y < -backWall && velocity.Y < 0) {
 		// ball is going in blue net
 		cvarManager->log("shot taken at blue net " + std::to_string(speed));
 		if (!orangeEnabled) {
@@ -238,23 +268,23 @@ void FastShots::setNewSpeed() {
 	speed += 0.5f;
 
 	if (setBothMin) {
-		cvarManager->executeCommand("fast_shots_min_blue " + std::to_string(speed));
-		cvarManager->executeCommand("fast_shots_min_orange " + std::to_string(speed));
+		cvarManager->executeCommand("fsgf_min_blue " + std::to_string(speed));
+		cvarManager->executeCommand("fsgf_min_orange " + std::to_string(speed));
 	} else if (setBothMax) {
-		cvarManager->executeCommand("fast_shots_max_blue " + std::to_string(speed));
-		cvarManager->executeCommand("fast_shots_max_orange " + std::to_string(speed));
+		cvarManager->executeCommand("fsgf_max_blue " + std::to_string(speed));
+		cvarManager->executeCommand("fsgf_max_orange " + std::to_string(speed));
 	} else if (ballLoc.Y < 0) {
 		if (setOrangeMin) {
-			cvarManager->executeCommand("fast_shots_min_orange " + std::to_string(speed));
+			cvarManager->executeCommand("fsgf_min_orange " + std::to_string(speed));
 		} else if (setOrangeMax) {
-			cvarManager->executeCommand("fast_shots_max_orange " + std::to_string(speed));
+			cvarManager->executeCommand("fsgf_max_orange " + std::to_string(speed));
 		}
 	} else if (ballLoc.Y > 0) {
 		if (setBlueMin) {
-			cvarManager->executeCommand("fast_shots_min_blue " + std::to_string(speed));
+			cvarManager->executeCommand("fsgf_min_blue " + std::to_string(speed));
 		}
 		else if (setBlueMax) {
-			cvarManager->executeCommand("fast_shots_max_blue " + std::to_string(speed));
+			cvarManager->executeCommand("fsgf_max_blue " + std::to_string(speed));
 		}
 	}
 }
